2476f77bd0ba24dacc05a8096f1a4516c40650ba
IoannaMi <ioannamicha@hotmail.com>
2020-09-30 20:54:55 +0200

    set the correct url for the back end

--- src/get-markerinfo.js
+++ src/get-markerinfo.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export default function (id) {
   return axios({
     method: 'post',
-    url: 'https://v-web018.external.intra/fewsprojectviewer/projectviewer/site/php_new/get_markerinfo.php',
+    url: 'https://fewsapplications.deltares.nl/data/php_new/get_markerinfo.php',
     data: { data: id },
     headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
   })
